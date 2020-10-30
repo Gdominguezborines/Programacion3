063bb9e1f80d59df6aa6a141f4bfdf817e800700
Gabrieldb <72223819+Gdominguezborines@users.noreply.github.com>
2020-10-30 12:50:55 +0100

    Update Ejercicio1.java

--- src/com/company/Boletin1/Ejercicio1.java
+++ src/com/company/Boletin1/Ejercicio1.java
@@ -4,7 +4,7 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         // write your code here
 
-        System.out.println("Aqui va el ejercicio 1");
+        System.out.println("Aqui va el  ejercicio 1");
     }
 }
 
