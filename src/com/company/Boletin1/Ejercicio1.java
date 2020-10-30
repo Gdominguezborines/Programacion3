19024b87a3a448c354d498e02207271be35bc243
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-10-30 11:19:08 +0100

    Primer commit

---
+++ src/com/company/Boletin1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.company.Boletin1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("Aqui va el ejercicio 1?");
+    }
+}
+
+
